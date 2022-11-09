c7e3dc940b335c1b3c7957a4b18b778e1417ed9d
izzdupont <91236027+izzdupont@users.noreply.github.com>
2022-11-09 16:03:49 -0600

    Update1 - Checkout Page
    Fix Checkout page
    -add subtotal option
    -connect checkout and amazon logo to the page
    -include reducer and StateProvider to render data to the data layer
    -the basket is updated by clicking button
    -subtotal is updating num of items and total$

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Checkout.css'
+import Subtotal from './Subtotal';
 
 function Checkout() {
   return (
     <div className='checkout'> 
-        <h1>Checkout Test chages</h1>
+      <div className='checkout__left'>
+        <img
+          className='checkout_ads'
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+      
+        <div>
+          <h2 className='checkout__title'>
+            Your Shopping Basket
+          </h2>
+        </div>
+      </div>
+
+      <div className='checkout__right'>
+        <Subtotal />
+      </div>
+
     </div>
   )
 }
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function Header() {
-  return (
+    const [ { basket }, dispatch] = useStateValue();
+
+    return (
     <div className='header'>
          {/* Amazon logo here*/}
-        <img
-          className="header__logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        />
+         <Link to="/">
+            <img
+            className="header__logo"
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            />
+         </Link>
+        
 
         <div className="header__search">
             <input className="header__searchInput" type="text" />
@@ -38,16 +45,18 @@ function Header() {
             </div>
 
              {/* Your basket */}
-            <div className="header__optionBasket">
-                <ShoppingBasketIcon />
-                <span className="header__optionLineTwo header__basketCount">
-                    0
-                </span>
-            </div>
+             <Link to="/checkout">
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">
+                        { basket?.length } {/* ? is optional chaining */}
+                    </span>
+                </div>
+            </Link>
         </div>
 
     </div>
   )
-}
+} 
 
 export default Header
